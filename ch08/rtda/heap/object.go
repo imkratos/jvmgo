f93192d0ba7b653f2d42d6af4f0fa7305dffa2a7
zhishuo <jingboshuo@gmail.com>
2018-01-27 19:23:24 +0800

    8章完

--- ch08/rtda/heap/object.go
+++ ch08/rtda/heap/object.go
@@ -29,3 +29,11 @@ func (self *Object) SetRefVar(name, descriptor string, ref *Object) {
 	slots := self.data.(Slots)
 	slots.SetRef(field.slotId, ref)
 }
+
+func (self *Object) GetRefVar(name, descriptor string) *Object {
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	return slots.GetRef(field.slotId)
+}
+
+
